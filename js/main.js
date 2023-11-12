62b6838ddc0997714f4dadc6539a55a322e5c04a
Rune <{ID}+{username}@users.noreply.github.com>
2023-11-12 09:50:36 +0100

    index

--- js/main.js
+++ js/main.js
@@ -7,49 +7,39 @@ class EventinatorApp {
 console.log("main.js loaded");
 let ea = new EventinatorApp();
 
-// Load the Google API client library
-gapi.load("client:auth2", start);
+/*
+ * Create form to request access token from Google's OAuth 2.0 server.
+ */
+function oauthSignIn() {
+    // Google's OAuth 2.0 endpoint for requesting an access token
+    var oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
 
-function start() {
-    gapi.client
-        .init({
-            apiKey: "AIzaSyDwyya24PHRbyx0fArS-eJJ6zCBV4OwSIA",
-            clientId:
-                "550349020422-m8h9na45b8abctrht36fsqadaueaqfrn.apps.googleusercontent.com",
-            discoveryDocs: [
-                "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
-            ],
-            scope: "https://www.googleapis.com/auth/calendar",
-            plugin_name: "calendar",
-        })
-        .then(function () {
-            // Sign in the user upon button click
-            gapi.auth2
-                .getAuthInstance()
-                .signIn()
-                .then(function () {
-                    // Define the calendar entry
-                    var event = {
-                        summary: "New Todo",
-                        start: {
-                            dateTime: "2023-12-01T09:00:00",
-                            timeZone: "America/Los_Angeles",
-                        },
-                        end: {
-                            dateTime: "2023-12-01T10:00:00",
-                            timeZone: "America/Los_Angeles",
-                        },
-                    };
+    // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+    var form = document.createElement("form");
+    form.setAttribute("method", "GET"); // Send as a GET request.
+    form.setAttribute("action", oauth2Endpoint);
 
-                    // Create the calendar entry
-                    var request = gapi.client.calendar.events.insert({
-                        calendarId: "primary",
-                        resource: event,
-                    });
+    // Parameters to pass to OAuth 2.0 endpoint.
+    var params = {
+        client_id:
+            "550349020422-m8h9na45b8abctrht36fsqadaueaqfrn.apps.googleusercontent.com",
+        redirect_uri: "https://r-poulsen.github.io/",
+        response_type: "token",
+        scope: "https://www.googleapis.com/auth/drive.metadata.readonly",
+        include_granted_scopes: "true",
+        state: "pass-through value",
+    };
 
-                    request.execute(function (event) {
-                        console.log("Event created: " + event.htmlLink);
-                    });
-                });
-        });
+    // Add form parameters as hidden input values.
+    for (var p in params) {
+        var input = document.createElement("input");
+        input.setAttribute("type", "hidden");
+        input.setAttribute("name", p);
+        input.setAttribute("value", params[p]);
+        form.appendChild(input);
+    }
+
+    // Add form to page and submit it to open the OAuth 2.0 endpoint.
+    document.body.appendChild(form);
+    form.submit();
 }
